66a5d3479d6ac8c3a5486900ba8c0e7320975050
segelia <133262062+segelia@users.noreply.github.com>
2023-11-09 14:49:02 +0100

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -55,16 +55,8 @@ Complex operator*(const Complex& a, const Complex& b) {
     return Complex((a.real * b.real) - (a.imag * b.imag), (a.real * b.imag) + (a.imag * b.real));
 }
 
-Complex operator/(const Complex& a, const Complex& b) {
-	// return a * b.conj() / b.norm2()
-	// Do powyższego zapisu potrzebujemy metody conj() która zwraca liczbę sprzężoną
-	// metody norm2 która zwraca kwadrat modułu liczby zespolonej (warto też napisać norm, która zwraca moduł)
-	// operator / który pozwala dzielić liczbę zespoloną przez liczbę rzeczywistą.
-	
+Complex operator/(const Complex& a, const Complex& b) {	
     double divisor = (b.real * b.real) + (b.imag * b.imag);
-    //~ double real_part = ((a.real * b.real) + (a.imag * b.imag)) / divisor;
-    //~ double imag_part = ((a.imag * b.real) - (a.real * b.imag)) / divisor;
-    //~ return Complex(real_part, imag_part);
     
 	return Complex(
 		((a.real * b.real) + (a.imag * b.imag)) / divisor,	// Real part.
@@ -84,18 +76,6 @@ std::ostream& operator<<(std::ostream& os, const Complex& c) {
     os << c.real << " + " << c.imag << "i";
     return os;
 }
-
-// Gdy piszemy:
-// cout << a << b << c;
-// to kompilator to rozumie jak:
-// ((cout << a) << b) << c;
-// Czyli wywołuje operator << na obiekcie "cout" który jest typu ostream (strumień danych)
-// do którego jest wrzucana wartość "a". To jak działa operator << na obiekcie ostream i obiekcie "a"
-// określa przeciążony operator <<. Wewnątrz tego operatora zwykle stosujemy
-// ten sam operator ale wyświetlamy dane, które ostream już potrafi obsłużyć, bo ma napisaną
-// obsługę tego operatora dla podstawowych typów. Na końcu dodajemy return on
-// aby na zwróconym obiekcie dało się ponownie wywołać operator << i wyświetlić obiekt b, a potem jeszcze
-// raz na zwróconym obiekcie wyświetlamy obiekt c.
 
 
 
